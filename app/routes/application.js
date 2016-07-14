732c5e75a7aaaf1576eee7a551326de8d981577d
Andy Edwards <andyhedwards@gmail.com>
2016-07-14 10:39:07 +1000

    Add invalidateSession action to application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-});
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+
+const { service } = Ember.inject;
+
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  actions: {
+    invalidateSession: function() {
+        this.get('session').invalidate();
+    }
+  }
+}
